16674348dde15b0d3868aac0ee469965f9aa2366
vidu2007 <91283115+vidu2007@users.noreply.github.com>
2022-01-30 19:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let word = 'Are you a good boy?';
-document.getElementById('text').innerHTML = word;
-let name = prompt("Are you a good boy?");
+// let word = 'Are you a good boy?';
+// document.getElementById('text').innerHTML = word;
+// let name = prompt("Are you a good boy?");
 
-if (name == 'yes') {
-  document.getElementById('text').innerHTML = 'ok good';
-}
+// if (name == 'yes') {
+//   document.getElementById('text').innerHTML = 'ok good';
+// }
   
-else {
-  document.getElementById('text').innerHTML = 'bad boy';
-}
+// else {
+//   document.getElementById('text').innerHTML = 'bad boy';
+// }
+
+let name = document.querySelector('#name');
+let p1 = document.querySelector('#p1');
+let p2 = document.querySelector('#p2');
+
+p2.setAttribute('style', 'display: none;');
